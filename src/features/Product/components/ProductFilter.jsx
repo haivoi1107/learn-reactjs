ef21ec311b451745d53c014ef53bef2f1b772939
HaiNguyen <haivoi1107@gmail.com>
2021-05-18 15:38:09 +0700

    commit

--- src/features/Product/components/ProductFilter.jsx
+++ src/features/Product/components/ProductFilter.jsx
@@ -2,27 +2,34 @@ import { Box } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import React from 'react';
 import FilterByCategory from './Filters/FilterByCategory';
+import FilterByPrice from './Filters/FilterByPrice';
 
 ProductFilter.propTypes = {
-    filters: PropTypes.object.isRequired,
-    onChange: PropTypes.func,
+  filters: PropTypes.object.isRequired,
+  onChange: PropTypes.func,
 };
 
-function ProductFilter({filters, onChange}) {
-    const handleCategoryChange = (newCategoryId) => {
-        if(!onChange) return;
-        const newFilter = {
-            ...filters,
-            'category.id' : newCategoryId,
-        };
-        onChange(newFilter);
-    }
-    return (
-        <Box>
-            <FilterByCategory onChange={handleCategoryChange} />
+function ProductFilter({ filters, onChange }) {
+  const handleCategoryChange = (newCategoryId) => {
+    if (!onChange) return;
+    const newFilter = {
+      'category.id': newCategoryId,
+    };
+    onChange(newFilter);
+  };
 
-        </Box>
-    );
+  const handlePriceChange = (values) => {
+    if (onChange) {
+      onChange(values);
+    }
+  };
+
+  return (
+    <Box>
+      <FilterByCategory onChange={handleCategoryChange} />
+      <FilterByPrice onChange={handlePriceChange} />
+    </Box>
+  );
 }
 
 export default ProductFilter;
