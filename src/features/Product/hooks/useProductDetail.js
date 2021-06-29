66d4f397fe39966e78cbf94ce909b5ccb0ff3dfa
HaiNguyen <haivoi1107@gmail.com>
2021-06-29 10:17:30 +0700

    commit

---
+++ src/features/Product/hooks/useProductDetail.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import productApi from "../../../api/productApi";
+
+
+export default function useProductDetail(productId) {
+    const [product, setProduct] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async() => {
+            try {
+                setLoading(true)
+                const result = await productApi.get(productId)
+                setProduct(result);
+                console.log(result);
+            } catch (error) {
+                console.log('False to fetch product', error);
+            }
+
+            setLoading(false);
+        })();
+
+
+    }, [productId])
+
+    return {product, loading}
+}
